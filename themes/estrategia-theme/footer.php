64f0088566d6da104bc5a3f6823bb5c0604a458c
Diego Mendoza <dmendoza@inflexionsoftware.com>
2016-02-24 22:15:39 -0600

    Se agregaron los links correctos a las redes sociales

--- themes/estrategia-theme/footer.php
+++ themes/estrategia-theme/footer.php
@@ -31,24 +31,24 @@
 						<!-- social networks -->
 						<ul id="social-list" class="list-unstyled text-center">
 							<li>
-								<a href="#">
+								<a href="https://www.facebook.com/preescolarsoya/?fref=ts" title="Siguenos en facebook">
 									<img src="<?php echo get_template_directory_uri(); ?>/images/icons/facebook.png" alt="Siguenos en facebook">			
 									<span>/estrategias-docentes</span>
 								</a>
 							</li>
 							<li>
-								<a href="#">
+								<a href="https://twitter.com/mis_estrategias" alt="Siguenos en twitter">
 									<img src="<?php echo get_template_directory_uri(); ?>/images/icons/twitter.png" alt="Siguenos en twitter">			
-									<span>@estrategias.docentes</span>
+									<span>@mis_estrategias</span>
 								</a>
 							</li>
-							<li>
+							<li class="hide">
 								<a href="#">
 									<img src="<?php echo get_template_directory_uri(); ?>/images/icons/linkedin.png" alt="Siguenos en linkedin">			
 									<span>/estrategias-docentes</span>
 								</a>
 							</li>
-							<li>
+							<li class="hide">
 								<a href="#">
 									<img src="<?php echo get_template_directory_uri(); ?>/images/icons/youtube.png" alt="Siguenos en youtube">			
 									<span>Estrategias docentes</span>
--- themes/estrategia-theme/social-header.php
+++ themes/estrategia-theme/social-header.php
@@ -1,20 +1,20 @@
 <ul class="list-unstyled list-inline">
 	<li>
-		<a href="#">
+		<a href="https://www.facebook.com/preescolarsoya/?fref=ts" title="Siguenos en facebook">
 			<img src="<?php echo get_template_directory_uri(); ?>/images/icons/facebook-wh.png" alt="Siguenos en facebook">			
 		</a>
 	</li>
 	<li>
-		<a href="#">
+		<a href="https://twitter.com/mis_estrategias" alt="Siguenos en twitter">
 			<img src="<?php echo get_template_directory_uri(); ?>/images/icons/twitter-wh.png" alt="Siguenos en twitter">			
 		</a>
 	</li>
-	<li>
+	<li class="hide">
 		<a href="#">
 			<img src="<?php echo get_template_directory_uri(); ?>/images/icons/linkedin-wh.png" alt="Siguenos en linkedin">
 		</a>
 	</li>
-	<li>
+	<li class="hide">
 		<a href="#">
 			<img src="<?php echo get_template_directory_uri(); ?>/images/icons/youtube-wh.png" alt="Siguenos en youtube">
 		</a>
