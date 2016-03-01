27429e76085b2d005a6845d5fd6b444dd63600f7
Diego Mendoza <dmendoza@inflexionsoftware.com>
2016-02-29 22:38:03 -0600

    Adecuaciones para que el diseño de las páginas de wordpress mantenga el mismo look and feel del site

--- themes/estrategia-theme/page.php
+++ themes/estrategia-theme/page.php
@@ -12,23 +12,17 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area col-md-8">
-		<main id="main" class="site-main" role="main">
+	<div id="primary-single" class="content-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+						<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() )
-						comments_template();
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+					<?php endwhile; // end of the loop. ?>
+				</div>
+			</div>
+		</div>
+	</div><!-- #content -->
 <?php get_footer(); ?>
